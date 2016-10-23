3ac51c6162b8bcccb38e0cf8661611081e471c6f
ecleveland <eric.cleveland9@live.com>
2016-10-23 15:27:39 -0500

    Adding Base WebAPI Project for testing

---
+++ webapp/dnsweb/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace dnsweb.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ webapp/dnsweb/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace dnsweb.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
