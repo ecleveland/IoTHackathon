310baee724c842127f560c75ab7e443ba35b978d
Eric Cleveland <eric.cleveland9@live.com>
2016-10-24 18:51:28 -0500

    Removed table - working from another computer

---
+++ webapp/AzureHelpers/AzureStorageHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace AzureHelpers
+{
+    public class AzureStorageHelper
+    {
+        private CloudStorageAccount storageAccount;
+        private const string connectionString = "DefaultEndpointsProtocol=https;AccountName=dnsstorageaccount;AccountKey=aPtxv4VfnuOLHnb6Y/iR6DZJ7Y7/bPQVoEPKjuvW6+U915op7XTo9CTRS2juKiZqr6N6CxyNXGNTW4WAfObwPw==";
+        public AzureStorageHelper()
+        {
+            storageAccount = CloudStorageAccount.Parse(connectionString);
+        }
+
+        public void GetAllTemperatures()
+        {
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            // Retrieve a reference to the table.
+            CloudTable table = tableClient.GetTableReference("DeviceRecords");
+
+            // Construct the query operation for all customer entities where PartitionKey="Smith".
+            //TableQuery<CustomerEntity> query = new TableQuery<CustomerEntity>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "Smith"));
+
+            // Print the fields for each customer.
+            //foreach (CustomerEntity entity in table.ExecuteQuery(query))
+            //{
+            //    Console.WriteLine("{0}, {1}\t{2}\t{3}", entity.PartitionKey, entity.RowKey,
+            //        entity.Email, entity.PhoneNumber);
+            //}
+        }
+    }
+}
