ccc2a59faa4f6a31abfec8bc8057bb1cc5f0ee6f
Eric Cleveland <eric.cleveland9@live.com>
2016-10-24 09:32:42 -0500

    Added PoC Functions / programs for device simulation

---
+++ PoC/SimulatedDevice1/SendCloudToDevice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+
+namespace SendCloudToDevice
+{
+    class Program
+    {
+        static ServiceClient serviceClient;
+        static string connectionString = "HostName=dns.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=Yz7BDPAyRCzHF63YWU6E2di30j8YPlepKkd5utT2Op0=";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Send Cloud-to-Device message\n");
+            serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+
+            Console.WriteLine("Press any key to send a C2D message.");
+            Console.ReadLine();
+            SendCloudToDeviceMessageAsync().Wait();
+            Console.ReadLine();
+        }
+
+        private async static Task SendCloudToDeviceMessageAsync()
+        {
+            var commandMessage = new Message(Encoding.ASCII.GetBytes("Cloud to device message."));
+            await serviceClient.SendAsync("myFirstDevice", commandMessage);
+        }
+    }
+}
---
+++ PoC/SimulatedDevice1/SimulatedDevice1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+
+namespace SimulatedDevice1
+{
+    class Program
+    {
+        // HostName=dns.azure-devices.net;DeviceId=simulatedDevice1;SharedAccessKey=h0Lc/c8Qyc1pORcSIDluH2tOyORxjyw7bFJwEwhz238=
+        static DeviceClient deviceClient;
+        static string iotHubUri = "dns.azure-devices.net";
+        static string deviceKey = "h0Lc/c8Qyc1pORcSIDluH2tOyORxjyw7bFJwEwhz238=";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simulated device\n");
+            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey("simulatedDevice1", deviceKey));
+
+            SendDeviceToCloudMessagesAsync();
+            ReceiveC2dAsync();
+            Console.ReadLine();
+        }
+
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            double avgWindSpeed = 10; // m/s
+            Random rand = new Random();
+            Console.WriteLine("\nDelivering telemetry.");
+            while (true)
+            {
+                double currentWindSpeed = avgWindSpeed + rand.NextDouble() * 4 - 2;
+
+                var telemetryDataPoint = new
+                {
+                    deviceId = "simulatedDevice1",
+                    windSpeed = currentWindSpeed
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                Task.Delay(1000).Wait();
+            }
+        }
+
+        private static async void ReceiveC2dAsync()
+        {
+            Console.WriteLine("\nReceiving cloud to device messages from service");
+            while (true)
+            {
+                Message receivedMessage = await deviceClient.ReceiveAsync();
+                if (receivedMessage == null) continue;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Received message: {0}", Encoding.ASCII.GetString(receivedMessage.GetBytes()));
+                Console.ResetColor();
+
+                await deviceClient.CompleteAsync(receivedMessage);
+            }
+        }
+    }
+}
