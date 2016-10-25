4916b3d4f603247db9ae60aba941d6f1ef75231e
ecleveland <eric.cleveland9@live.com>
2016-10-25 15:31:20 -0500

    Added two full prototype applications.

---
+++ PoC/SimulatedDevice1/adafruitProto/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace adafruitProto
+{
+    class Program
+    {
+        static DeviceClient deviceClient;
+        static string iotHubUri = "dns.azure-devices.net";
+        static string deviceKey = "K6bzg7DGX8D6r9LUTkNAyPzw71MDFRlymSU7MDHmjV4=";
+        static string deviceId = "adafruitProto1";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simulated device\n");
+            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey));
+
+            SendDeviceToCloudMessagesAsync();
+            Console.ReadLine();
+        }
+
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            // Sogeti Dallas : 32.8712126,-96.9457513
+            // Bellevue Park : 47.6124876,-122.2048304
+            // Globe Life Park : 32.7512847,-97.0846984
+            double lat = 47.6124876;
+            double lon = -122.2048304;
+            Random rand = new Random();
+            Console.WriteLine("\nDelivering telemetry.");
+            while (true)
+            {
+                double currentLat = lat + rand.NextDouble() * .001;
+                double currentLong = lon + rand.NextDouble() * .001;
+
+                var telemetryDataPoint = new
+                {
+                    DeviceId = deviceId,
+                    Latitude = currentLat,
+                    Longitude = currentLong,
+                    EventTime = DateTime.Now,
+                    Pressure = 1023,
+                    MTemperature = 24,
+                    Humidity = 45
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                Task.Delay(5000).Wait();
+            }
+        }
+    }
+}
